468324dca8f708d9f15d40cd69d6e5dcb453d3cf
MandanaTajoddin <40220065+MandanaTajoddin@users.noreply.github.com>
2018-06-27 13:19:48 -0400

    changed index-main

---
+++ main.js
@@ -0,0 +1,12 @@
+let name = prompt("What\'s your name?");
+console.log(name);
+let origin = prompt("Where are you from " + name);
+let resident = prompt("Are you a lawful resident of US " + name);
+let date = prompt("When did you enter US from " + origin);
+let job = prompt("What is your educational degree " + name);
+
+console.log(origin);
+console.log(resident);
+console.log(date);
+console.log(job);
+console.log(alert("Let\'s go " + name));
