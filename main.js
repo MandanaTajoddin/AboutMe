ac61cefca7ced07bd3d5d01c664281c46fd87db6
MandanaTajoddin <40220065+MandanaTajoddin@users.noreply.github.com>
2018-12-09 23:40:00 -0500

    changed

--- main.js
+++ main.js
@@ -1,11 +1,34 @@
-let name = prompt("What\'s your name?");
-console.log(name);
-let origin = prompt("Where are you from " + name);
-let resident = prompt("Are you a lawful resident of US " + name);
-let date = prompt("When did you enter US from " + origin);
-let job = prompt("What is your educational degree " + name);
-console.log(origin);
-console.log(resident);
-console.log(date);
-console.log(job);
-console.log(alert("Let\'s go " + name));
+let checkArr = ['ASkj2','5EwuN', 'DrEVo' , 'KjIPP']
+let randomText = Math.floor(Math.random() * checkArr.length);
+let checkText = checkArr[randomText];
+let checkHuman = prompt('Please type ' + checkText );
+alert('Hi welcome to this website let\'s go!');
+let myName = prompt('what\'s your name?');
+console.log(myName);
+
+let myAge = confirm('Are you older than 18 years old?');
+console.log(myAge);
+
+if(myAge){
+    alert('Hello '+ myName + ' you are eligible for uploading your resume');
+}else {
+    alert('Hello '+ myName + ' you are so young we cannot accept you');
+}
+
+let myOrigin = prompt('Where are you from ' + myName);
+console.log(myOrigin);
+
+let myId = confirm('Do you have a valid driver licence?');
+console.log(myId);
+
+let myJob = prompt('What was you previos job ' + myName + ' in ' + myOrigin);
+console.log(myJob);
+
+let myArray= [' White', 'Hispanic or Latino', 'African-American' ,'East Asia']
+let captcha = prompt('Please write your race ' + myArray)
+
+alert('let\'s go ' + myName + ' !');
+
+if(myName !== null){
+    document.getElementById('myName').innerHTML = myName;
+}
